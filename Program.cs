8e19f221fb42d1d1ec40a56c0d700788a012d1b5
Betim <dev.herberth@gmail.com>
2023-11-08 20:45:36 -0300

    main: editor config included

--- Program.cs
+++ Program.cs
@@ -1,76 +1,83 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<ApplicationDbContext>();
+
 var app = builder.Build();
 var configuration = app.Configuration;
 ProductRepository.Init(configuration);
 
-app.MapPost("/products", (Product product) => {
+app.MapPost("/products", (Product product) =>
+{
   ProductRepository.Add(product);
   return Results.Created($"/products/{product.Code}", product);
 });
 
-app.MapGet("/products/{code}", ([FromRoute] string code) => {
+app.MapGet("/products/{code}", ([FromRoute] string code) =>
+{
   var product = ProductRepository.GetBy(code);
 
-  if (product != null) return Results.Ok(product);
-  return Results.NotFound();
+  return product != null ? Results.Ok(product) : Results.NotFound();
 });
 
-app.MapGet("/products", () => {
+app.MapGet("/products", () =>
+{
   var products = ProductRepository.GetAll();
 
-  if (products != null) return Results.Ok(products);
-  return Results.NotFound();
+  return products != null ? Results.Ok(products) : Results.NotFound();
 });
 
-app.MapPut("/products", (Product product) => {
+app.MapPut("/products", (Product product) =>
+{
   ProductRepository.Edit(product);
 
   return Results.Ok(product);
 });
 
-app.MapDelete("/products/{code}", ([FromRoute] string code) => {
+app.MapDelete("/products/{code}", ([FromRoute] string code) =>
+{
   ProductRepository.Delete(code);
 
   return Results.Ok();
 });
-if(app.Environment.IsStaging()) {
-  app.MapGet("/configuration/database", (IConfiguration configuration) => {
-    return Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}");
-  });
+if (app.Environment.IsStaging())
+{
+  app.MapGet("/configuration/database", (IConfiguration configuration) => Results.Ok($"{configuration["database:connection"]}/{configuration["database:port"]}"));
 }
 
 app.Run();
 
-public static class ProductRepository {
+public static class ProductRepository
+{
   public static List<Product>? Products { get; set; } = Products = [];
 
-  public static void Init(IConfiguration configuration) {
+  public static void Init(IConfiguration configuration)
+  {
     var products = configuration.GetSection("Products").Get<List<Product>>();
-
     if (products != null) Products = products;
-
     Products = products;
   }
 
-  public static void Add(Product product) {
+  public static void Add(Product product)
+  {
     Products ??= [];
 
     Products.Add(product);
   }
 
-  public static Product? GetBy(string code) {
+  public static Product? GetBy(string code)
+  {
     return Products?.FirstOrDefault(p => p.Code == code);
   }
 
-  public static List<Product>? GetAll() {
-    if (Products == null) return null;
-
-    return Products.FindAll(p => p.Code != null);
+  public static List<Product>? GetAll()
+  {
+    return Products?.FindAll(p => p.Code != null);
   }
 
-  public static void Edit(Product product) {
+  public static void Edit(Product product)
+  {
     if (product.Code == null) return;
 
     var productToEdit = GetBy(product.Code);
@@ -81,7 +88,8 @@ public static class ProductRepository {
     productToEdit.Price = product.Price;
   }
 
-  public static void Delete(string code) {
+  public static void Delete(string code)
+  {
     if (code == null) return;
 
     var productToDelete = GetBy(code);
@@ -92,8 +100,17 @@ public static class ProductRepository {
   }
 }
 
-public class Product {
+public class Product
+{
   public string? Code { get; set; }
+
   public string? Name { get; set; }
   public decimal Price { get; set; }
+}
+
+public class ApplicationDbContext : DbContext
+{
+  public DbSet<Product> Products { get; set; }
+  protected override void OnConfiguring(DbContextOptionsBuilder options) => options
+    .UseSqlServer("Server=localhost;Database=Products;User Id=sa;Password=sql@2023;MultipleActiveResultSets=true;Encrypt=yes;TrustServerCertificate=yes;");
 }
