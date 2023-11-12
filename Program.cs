77cb7eb8875026ee15c3c623240eeb9c5b26ab47
Betim <dev.herberth@gmail.com>
2023-11-12 16:17:28 -0300

    develop: Add SQL Server configs && moved types

--- Program.cs
+++ Program.cs
@@ -1,8 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["Database:SqlServer"]);
 
 var app = builder.Build();
 var configuration = app.Configuration;
@@ -47,97 +46,3 @@ if (app.Environment.IsStaging())
 }
 
 app.Run();
-
-public static class ProductRepository
-{
-  public static List<Product>? Products { get; set; } = Products = [];
-
-  public static void Init(IConfiguration configuration)
-  {
-    var products = configuration.GetSection("Products").Get<List<Product>>();
-    if (products != null) Products = products;
-    Products = products;
-  }
-
-  public static void Add(Product product)
-  {
-    Products ??= [];
-
-    Products.Add(product);
-  }
-
-  public static Product? GetBy(string code)
-  {
-    return Products?.FirstOrDefault(p => p.Code == code);
-  }
-
-  public static List<Product>? GetAll()
-  {
-    return Products?.FindAll(p => p.Code != null);
-  }
-
-  public static void Edit(Product product)
-  {
-    if (product.Code == null) return;
-
-    var productToEdit = GetBy(product.Code);
-
-    if (productToEdit == null) return;
-
-    productToEdit.Name = product.Name;
-    productToEdit.Price = product.Price;
-  }
-
-  public static void Delete(string code)
-  {
-    if (code == null) return;
-
-    var productToDelete = GetBy(code);
-
-    if (productToDelete == null) return;
-
-    Products?.Remove(productToDelete);
-  }
-}
-
-public class Category
-{
-  public int Id { get; set; }
-  public string? Name { get; set; }
-}
-
-public class Tag
-{
-  public int Id { get; set; }
-  public string? Name { get; set; }
-  public int ProductId { get; set; }
-}
-
-public class Product
-{
-  public int Id { get; set; }
-  public string? Code { get; set; }
-  public string? Name { get; set; }
-  public decimal Price { get; set; }
-  public string? Description { get; set; }
-  public int CategoryId { get; set; }
-  public Category? Category { get; set; }
-  public List<Tag>? Tag { get; set; }
-}
-
-public class ApplicationDbContext : DbContext
-{
-  public DbSet<Product> Products { get; set; }
-
-  protected override void OnModelCreating(ModelBuilder modelBuilder)
-  {
-    modelBuilder.Entity<Product>()
-      .Property(p => p.Description).HasMaxLength(500).IsRequired(false);
-    modelBuilder.Entity<Product>()
-      .Property(p => p.Name).HasMaxLength(120).IsRequired();
-    modelBuilder.Entity<Product>()
-      .Property(p => p.Code).HasMaxLength(20).IsRequired();
-  }
-  protected override void OnConfiguring(DbContextOptionsBuilder options) => options
-    .UseSqlServer("Server=localhost;Database=Products;User Id=sa;Password=<sql@2023>;MultipleActiveResultSets=true;Encrypt=yes;TrustServerCertificate=yes;");
-}
--- obj/Debug/net8.0/api.AssemblyInfo.cs
+++ obj/Debug/net8.0/api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b9460093e13d15a917a1aff1c780bfdfb926dc37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f11b701c1e8c5728bdda2d683d39e19fd9c780e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
